3cbe5c46b5a8905024cd69225c1a085a2c723a97
Jean-Yves Tinevez <tinevez@pasteur.fr>
2022-01-13 17:47:34 +0100

    Enable the 'cyto2' model.

--- src/main/java/fiji/plugin/trackmate/cellpose/CellposeSettings.java
+++ src/main/java/fiji/plugin/trackmate/cellpose/CellposeSettings.java
@@ -223,6 +223,7 @@ public class CellposeSettings
 	{
 		CYTO( "Cytoplasm", "cyto" ),
 		NUCLEI( "Nucleus", "nuclei" ),
+		CYTO2( "Cytoplasm 2.0", "cyto2" ),
 		CUSTOM( "Custom", "" );
 
 		private final String name;
