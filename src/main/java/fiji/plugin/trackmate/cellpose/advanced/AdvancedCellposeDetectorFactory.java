f8ba2c6e0b6ba5827fb052b861209335aa5db438
Jean-Yves Tinevez <tinevez@pasteur.fr>
2025-07-17 15:22:39 +0200

    Use the new Configurator thingie for the the advanced cellpose detector.
    Only two classes. Nice!

--- src/main/java/fiji/plugin/trackmate/cellpose/advanced/AdvancedCellposeDetectorFactory.java
+++ src/main/java/fiji/plugin/trackmate/cellpose/advanced/AdvancedCellposeDetectorFactory.java
@@ -21,141 +21,36 @@
  */
 package fiji.plugin.trackmate.cellpose.advanced;
 
-import static fiji.plugin.trackmate.detection.DetectorKeys.KEY_TARGET_CHANNEL;
-import static fiji.plugin.trackmate.detection.ThresholdDetectorFactory.KEY_SIMPLIFY_CONTOURS;
-import static fiji.plugin.trackmate.io.IOUtils.readBooleanAttribute;
-import static fiji.plugin.trackmate.io.IOUtils.readDoubleAttribute;
-import static fiji.plugin.trackmate.io.IOUtils.readIntegerAttribute;
-import static fiji.plugin.trackmate.io.IOUtils.readStringAttribute;
-import static fiji.plugin.trackmate.io.IOUtils.writeAttribute;
-import static fiji.plugin.trackmate.util.TMUtils.checkMapKeys;
-import static fiji.plugin.trackmate.util.TMUtils.checkParameter;
-
-import java.util.Arrays;
-import java.util.List;
 import java.util.Map;
 
 import javax.swing.ImageIcon;
 
-import org.jdom2.Element;
 import org.scijava.Priority;
 import org.scijava.plugin.Plugin;
 
-import fiji.plugin.trackmate.Logger;
-import fiji.plugin.trackmate.Model;
-import fiji.plugin.trackmate.Settings;
+import fiji.plugin.trackmate.cellpose.CellposeCLI;
 import fiji.plugin.trackmate.cellpose.CellposeDetector;
-import fiji.plugin.trackmate.cellpose.CellposeDetectorFactory;
-import fiji.plugin.trackmate.cellpose.CellposeSettings.PretrainedModelCellpose;
+import fiji.plugin.trackmate.cellpose.CellposeUtils;
 import fiji.plugin.trackmate.detection.SpotDetectorFactory;
-import fiji.plugin.trackmate.detection.SpotDetectorFactoryBase;
+import fiji.plugin.trackmate.detection.SpotDetectorFactoryGenericConfig;
 import fiji.plugin.trackmate.detection.SpotGlobalDetector;
-import fiji.plugin.trackmate.io.IOUtils;
-import fiji.plugin.trackmate.util.TMUtils;
+import fiji.plugin.trackmate.detection.SpotGlobalDetectorFactory;
+import fiji.plugin.trackmate.util.cli.TrackMateSettingsBuilder;
+import ij.ImagePlus;
+import net.imagej.ImgPlus;
 import net.imglib2.Interval;
 import net.imglib2.type.NativeType;
 import net.imglib2.type.numeric.RealType;
 
 @Plugin( type = SpotDetectorFactory.class, priority = Priority.LOW - 1. )
-public class AdvancedCellposeDetectorFactory< T extends RealType< T > & NativeType< T > > extends CellposeDetectorFactory< T >
+public class AdvancedCellposeDetectorFactory< T extends RealType< T > & NativeType< T > >
+		implements SpotGlobalDetectorFactory< T >, SpotDetectorFactoryGenericConfig< T, AdvancedCellposeCLI >
+
 {
 
 	/*
 	 * CONSTANTS
 	 */
-
-	/**
-	 * The key to the parameter that store the flow threshold value. From
-	 * cellpose docs:
-	 * <p>
-	 * Note there is nothing keeping the neural network from predicting
-	 * horizontal and vertical flows that do not correspond to any real shapes
-	 * at all. In practice, most predicted flows are consistent with real
-	 * shapes, because the network was only trained on image flows that are
-	 * consistent with real shapes, but sometimes when the network is uncertain
-	 * it may output inconsistent flows. To check that the recovered shapes
-	 * after the flow dynamics step are consistent with real ROIs, we recompute
-	 * the flow gradients for these putative predicted ROIs, and compute the
-	 * mean squared error between them and the flows predicted by the network.
-	 * <p>
-	 * The flow_threshold parameter is the maximum allowed error of the flows
-	 * for each mask. The default is flow_threshold=0.4. Increase this threshold
-	 * if cellpose is not returning as many ROIs as you’d expect. Similarly,
-	 * decrease this threshold if cellpose is returning too many ill-shaped
-	 * ROIs.
-	 */
-	public static final String KEY_FLOW_THRESHOLD = "FLOW_THRESHOLD";
-
-	public static final Double DEFAULT_FLOW_THRESHOLD = Double.valueOf( 0.4 );
-
-	/**
-	 * The key to the parameter that store the cell probability threshold value.
-	 * From cellpose docs:
-	 * <p>
-	 * The network predicts 3 outputs: flows in X, flows in Y, and cell
-	 * “probability”. The predictions the network makes of the probability are
-	 * the inputs to a sigmoid centered at zero (1 / (1 + e^-x)), so they vary
-	 * from around -6 to +6. The pixels greater than the cellprob_threshold are
-	 * used to run dynamics and determine ROIs. The default is
-	 * cellprob_threshold=0.0. Decrease this threshold if cellpose is not
-	 * returning as many ROIs as you’d expect. Similarly, increase this
-	 * threshold if cellpose is returning too ROIs particularly from dim areas.
-	 */
-	public static final String KEY_CELL_PROB_THRESHOLD = "CELL_PROB_THRESHOLD";
-
-	public static final Double DEFAULT_CELL_PROB_THRESHOLD = Double.valueOf( 0. );
-
-	/**
-	 * The key to the parameter that store the minimum size to keep masks. Used
-	 * only if do_3D mode or 2D+Z and stitch_threshold > 0 From cellpose docs:
-	 * <p>
-	 * Minimum number of pixels per mask, can turn off with -1.
-	 */
-	public static final String KEY_CELL_MIN_SIZE = "CELL_MIN_SIZE";
-
-	public static final Double DEFAULT_CELL_MIN_SIZE = Double.valueOf( 15. );
-
-	/**
-	 * The key to the parameter that store the resampling option. From cellpose
-	 * docs:
-	 * <p>
-	 * The cellpose network is run on your rescaled image – where the rescaling
-	 * factor is determined by the diameter you input (or determined
-	 * automatically as above). For instance, if you have an image with 60 pixel
-	 * diameter cells, the rescaling factor is 30./60. = 0.5. After determining
-	 * the flows (dX, dY, cellprob), the model runs the dynamics. The dynamics
-	 * can be run at the rescaled size (resample=False), or the dynamics can be
-	 * run on the resampled, interpolated flows at the true image size
-	 * (resample=True). resample=True will create smoother ROIs when the cells
-	 * are large but will be slower in case; resample=False will find more ROIs
-	 * when the cells are small but will be slower in this case. By default in
-	 * versions >=1.0 resample=True.
-	 */
-	public static final Boolean DEFAULT_RESAMPLE = true;
-
-	public static final String KEY_RESAMPLE = "RESAMPLE";
-
-	/**
-	 * Parameters for CellPose 3D mode: either do_3D (do xy, yz, zx) or
-	 * 2D+stitch_threshold to reconstruct in 3D from cellpose docs:
-	 * <p>
-	 * There may be additional differences in YZ and XZ slices that make them
-	 * unable to be used for 3D segmentation. I’d recommend viewing the volume
-	 * in those dimensions if the segmentation is failing. In those instances,
-	 * you may want to turn off 3D segmentation (do_3D=False) and run instead
-	 * with stitch_threshold>0. Cellpose will create ROIs in 2D on each XY slice
-	 * and then stitch them across slices if the IoU between the mask on the
-	 * current slice and the next slice is greater than or equal to the
-	 * stitch_threshold.
-	 */
-	public static final Boolean DEFAULT_DO2DZ = false;
-
-	public static final String KEY_DO2DZ = "DO2DZ";
-
-	/** Default value of iou threshold for 2D+z stitching */
-	public static final Double DEFAULT_IOU_THRESHOLD = Double.valueOf( 0.25 );
-
-	public static final String KEY_IOU_THRESHOLD = "IOUTHRESHOLD";
 
 	/** A string key identifying this factory. */
 	public static final String DETECTOR_KEY = "CELLPOSE_ADVANCED_DETECTOR";
@@ -188,198 +83,23 @@ public class AdvancedCellposeDetectorFactory< T extends RealType< T > & NativeTy
 	 */
 
 	@Override
-	public SpotGlobalDetector< T > getDetector( final Interval interval )
+	public AdvancedCellposeCLI getConfigurator( final ImagePlus imp )
 	{
-		// Base settings.
+		final int nChannels = ( imp == null ) ? 1 : imp.getNChannels();
+		final String units = ( imp == null ) ? "no input image" : imp.getCalibration().getUnit();
+		final double pixelSize = ( imp == null ) ? 1. : imp.getCalibration().pixelWidth;
+		return new AdvancedCellposeCLI( nChannels, units, pixelSize );
+	}
 
-		final String cellposePythonPath = ( String ) settings.get( KEY_CELLPOSE_PYTHON_FILEPATH );
-		final PretrainedModelCellpose model = ( PretrainedModelCellpose ) settings.get( KEY_CELLPOSE_MODEL );
-		final String customModelPath = ( String ) settings.get( KEY_CELLPOSE_CUSTOM_MODEL_FILEPATH );
-		final boolean simplifyContours = ( boolean ) settings.get( KEY_SIMPLIFY_CONTOURS );
-		final boolean useGPU = ( boolean ) settings.get( KEY_USE_GPU );
-
-		// Channels are 0-based (0: grayscale, then R & G & B).
-		final int channel = ( Integer ) settings.get( KEY_TARGET_CHANNEL );
-		final int channel2 = ( Integer ) settings.get( KEY_OPTIONAL_CHANNEL_2 );
-
-		// Convert to diameter in pixels.
-		final double[] calibration = TMUtils.getSpatialCalibration( img );
-		final double diameter = ( double ) settings.get( KEY_CELL_DIAMETER ) / calibration[ 0 ];
-
-		// Advanced settings.
-
-		final double flowThreshold = ( Double ) settings.get( KEY_FLOW_THRESHOLD );
-		final double cellProbThreshold = ( Double ) settings.get( KEY_CELL_PROB_THRESHOLD );
-//		final boolean do2DZ = ( Boolean ) settings.get( KEY_DO2DZ );
-//		final double iouThreshold = ( Double ) settings.get( KEY_IOU_THRESHOLD );
-		final boolean resample = ( Boolean ) settings.get( KEY_RESAMPLE );
-		// min size: get rid of masks with few pixels in CP (but done on
-		// original image size, so possible bias of anisotropy)
-		final double cellMinSize = ( double ) settings.get( KEY_CELL_MIN_SIZE ); // in
-																					// pixels
-
-		final AdvancedCellposeSettings cellposeSettings = AdvancedCellposeSettings
-				.create()
-				.cellposePythonPath( cellposePythonPath )
-				.customModel( customModelPath )
-				.model( model )
-				.channel1( channel )
-				.channel2( channel2 )
-				.diameter( diameter )
-				.useGPU( useGPU )
-				.simplifyContours( simplifyContours )
-				.flowThreshold( flowThreshold )
-				.cellProbThreshold( cellProbThreshold )
-				.cellMinSize( cellMinSize )
-//				.do2DZ( do2DZ )
-//				.iouThreshold( iouThreshold )
-				.resample( resample )
-				.get();
-
-		// Logger.
-		final Logger logger = ( Logger ) settings.get( KEY_LOGGER );
-		final CellposeDetector< T > detector = new CellposeDetector<>( img, interval, cellposeSettings, logger );
+	@Override
+	public SpotGlobalDetector< T > getDetector( final ImgPlus< T > img, final Map< String, Object > settings, final Interval interval )
+	{
+		// Create the CLI and loads settings into it.
+		final CellposeCLI cli = getConfigurator( img );
+		TrackMateSettingsBuilder.fromTrackMateSettings( settings, cli );
+		// Create the detector.
+		final CellposeDetector< T > detector = new CellposeDetector<>( img, interval, cli );
 		return detector;
-	}
-
-	@Override
-	public boolean marshall( final Map< String, Object > settings, final Element element )
-	{
-		if ( !super.marshall( settings, element ) )
-			return false;
-
-		final StringBuilder errorHolder = new StringBuilder();
-		boolean ok = writeAttribute( settings, element, KEY_FLOW_THRESHOLD, Double.class, errorHolder );
-		ok = ok && writeAttribute( settings, element, KEY_CELL_PROB_THRESHOLD, Double.class, errorHolder );
-		ok = ok && writeAttribute( settings, element, KEY_CELL_MIN_SIZE, Double.class, errorHolder );
-//		ok = ok && writeAttribute( settings, element, KEY_DO2DZ, Boolean.class, errorHolder );
-//		ok = ok && writeAttribute( settings, element, KEY_IOU_THRESHOLD, Double.class, errorHolder );
-		ok = ok && writeAttribute( settings, element, KEY_RESAMPLE, Boolean.class, errorHolder );
-		if ( !ok )
-			errorMessage = errorHolder.toString();
-		return ok;
-	}
-
-	@Override
-	public boolean unmarshall( final Element element, final Map< String, Object > settings )
-	{
-		settings.clear();
-		final StringBuilder errorHolder = new StringBuilder();
-		boolean ok = true;
-		ok = ok && readStringAttribute( element, settings, KEY_CELLPOSE_PYTHON_FILEPATH, errorHolder );
-		ok = ok && readStringAttribute( element, settings, KEY_CELLPOSE_CUSTOM_MODEL_FILEPATH, errorHolder );
-		ok = ok && readIntegerAttribute( element, settings, KEY_TARGET_CHANNEL, errorHolder );
-		ok = ok && readIntegerAttribute( element, settings, KEY_OPTIONAL_CHANNEL_2, errorHolder );
-		ok = ok && readDoubleAttribute( element, settings, KEY_CELL_DIAMETER, errorHolder );
-		ok = ok && readBooleanAttribute( element, settings, KEY_USE_GPU, errorHolder );
-		ok = ok && readBooleanAttribute( element, settings, KEY_SIMPLIFY_CONTOURS, errorHolder );
-		ok = ok && readDoubleAttribute( element, settings, KEY_FLOW_THRESHOLD, errorHolder );
-		ok = ok && readDoubleAttribute( element, settings, KEY_CELL_PROB_THRESHOLD, errorHolder );
-		ok = ok && readDoubleAttribute( element, settings, KEY_CELL_MIN_SIZE, errorHolder );
-//		ok = ok && readBooleanAttribute( element, settings, KEY_DO2DZ, errorHolder );
-//		ok = ok && readDoubleAttribute( element, settings, KEY_IOU_THRESHOLD, errorHolder );
-		ok = ok && readBooleanAttribute( element, settings, KEY_RESAMPLE, errorHolder );
-
-		// Read model.
-		final String str = element.getAttributeValue( KEY_CELLPOSE_MODEL );
-		if ( null == str )
-		{
-			errorHolder.append( "Attribute " + KEY_CELLPOSE_MODEL + " could not be found in XML element.\n" );
-			ok = false;
-		}
-		settings.put( KEY_CELLPOSE_MODEL, PretrainedModelCellpose.valueOf( str ) );
-
-		return checkSettings( settings );
-	}
-
-	@Override
-	public AdvancedCellposeDetectorConfigurationPanel getDetectorConfigurationPanel( final Settings settings, final Model model )
-	{
-		return new AdvancedCellposeDetectorConfigurationPanel( settings, model );
-	}
-
-	@Override
-	public Map< String, Object > getDefaultSettings()
-	{
-		final Map< String, Object > settings = super.getDefaultSettings();
-		settings.put( KEY_FLOW_THRESHOLD, DEFAULT_FLOW_THRESHOLD );
-		settings.put( KEY_CELL_PROB_THRESHOLD, DEFAULT_CELL_PROB_THRESHOLD );
-		settings.put( KEY_CELL_MIN_SIZE, DEFAULT_CELL_MIN_SIZE );
-//		settings.put( KEY_DO2DZ, DEFAULT_DO2DZ );
-//		settings.put( KEY_IOU_THRESHOLD, DEFAULT_IOU_THRESHOLD );
-		settings.put( KEY_RESAMPLE, DEFAULT_RESAMPLE );
-		return settings;
-	}
-
-	@Override
-	public boolean checkSettings( final Map< String, Object > settings )
-	{
-		boolean ok = true;
-		final StringBuilder errorHolder = new StringBuilder();
-		ok = ok & checkParameter( settings, KEY_CELLPOSE_PYTHON_FILEPATH, String.class, errorHolder );
-		ok = ok & checkParameter( settings, KEY_CELLPOSE_CUSTOM_MODEL_FILEPATH, String.class, errorHolder );
-		ok = ok & checkParameter( settings, KEY_CELLPOSE_MODEL, PretrainedModelCellpose.class, errorHolder );
-		ok = ok & checkParameter( settings, KEY_TARGET_CHANNEL, Integer.class, errorHolder );
-		ok = ok & checkParameter( settings, KEY_OPTIONAL_CHANNEL_2, Integer.class, errorHolder );
-		ok = ok & checkParameter( settings, KEY_CELL_DIAMETER, Double.class, errorHolder );
-		ok = ok & checkParameter( settings, KEY_USE_GPU, Boolean.class, errorHolder );
-		ok = ok & checkParameter( settings, KEY_SIMPLIFY_CONTOURS, Boolean.class, errorHolder );
-		ok = ok & checkParameter( settings, KEY_FLOW_THRESHOLD, Double.class, errorHolder );
-		ok = ok & checkParameter( settings, KEY_CELL_PROB_THRESHOLD, Double.class, errorHolder );
-		ok = ok & checkParameter( settings, KEY_CELL_MIN_SIZE, Double.class, errorHolder );
-//		ok = ok & checkParameter( settings, KEY_DO2DZ, Boolean.class, errorHolder );
-//		ok = ok & checkParameter( settings, KEY_IOU_THRESHOLD, Double.class, errorHolder );
-		ok = ok & checkParameter( settings, KEY_RESAMPLE, Boolean.class, errorHolder );
-//		ok = ok & checkOptionalParameter( settings, KEY_SMOOTHING_SCALE, Double.class, errorHolder );
-
-		// If we have a logger, test it is of the right class.
-		final Object loggerObj = settings.get( KEY_LOGGER );
-		if ( loggerObj != null && !Logger.class.isInstance( loggerObj ) )
-		{
-			errorHolder.append( "Value for parameter " + KEY_LOGGER + " is not of the right class. "
-					+ "Expected " + Logger.class.getName() + ", got " + loggerObj.getClass().getName() + ".\n" );
-			ok = false;
-		}
-
-		final List< String > mandatoryKeys = Arrays.asList(
-				KEY_CELLPOSE_PYTHON_FILEPATH,
-				KEY_CELLPOSE_MODEL,
-				KEY_TARGET_CHANNEL,
-				KEY_OPTIONAL_CHANNEL_2,
-				KEY_CELL_DIAMETER,
-				KEY_USE_GPU,
-				KEY_SIMPLIFY_CONTOURS );
-		final List< String > optionalKeys = Arrays.asList(
-				KEY_CELLPOSE_CUSTOM_MODEL_FILEPATH,
-				KEY_LOGGER,
-				KEY_FLOW_THRESHOLD,
-				KEY_CELL_PROB_THRESHOLD,
-				KEY_CELL_MIN_SIZE,
-//				KEY_DO2DZ,
-//				KEY_IOU_THRESHOLD,
-				KEY_RESAMPLE );
-//				KEY_SMOOTHING_SCALE );
-		ok = ok & checkMapKeys( settings, mandatoryKeys, optionalKeys, errorHolder );
-		if ( !ok )
-			errorMessage = errorHolder.toString();
-
-		// Extra test to make sure we can read the classifier file.
-		if ( ok )
-		{
-			final Object obj = settings.get( KEY_CELLPOSE_PYTHON_FILEPATH );
-			if ( obj == null )
-			{
-				errorMessage = "The path to the Cellpose python executable is not set.";
-				return false;
-			}
-
-			if ( !IOUtils.canReadFile( ( String ) obj, errorHolder ) )
-			{
-				errorMessage = "Problem with Cellpose python executable: " + errorHolder.toString();
-				return false;
-			}
-		}
-		return ok;
 	}
 
 	@Override
@@ -391,7 +111,7 @@ public class AdvancedCellposeDetectorFactory< T extends RealType< T > & NativeTy
 	@Override
 	public ImageIcon getIcon()
 	{
-		return null;
+		return CellposeUtils.cellposeLogo64();
 	}
 
 	@Override
@@ -407,14 +127,14 @@ public class AdvancedCellposeDetectorFactory< T extends RealType< T > & NativeTy
 	}
 
 	@Override
-	public boolean has2Dsegmentation()
+	public String getUrl()
 	{
-		return true;
+		return DOC_ADV_CELLPOSE_URL;
 	}
 
 	@Override
-	public SpotDetectorFactoryBase< T > copy()
+	public boolean has2Dsegmentation()
 	{
-		return new AdvancedCellposeDetectorFactory<>();
+		return true;
 	}
 }
