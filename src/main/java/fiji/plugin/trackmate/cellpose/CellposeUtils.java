acbd633437f80146caa8eed85393d1a59584cbc7
Jean-Yves TINEVEZ <tinevez@pasteur.fr>
2025-08-19 11:41:36 +0200

    Migrate to and use utilities in static classes.

--- src/main/java/fiji/plugin/trackmate/cellpose/CellposeUtils.java
+++ src/main/java/fiji/plugin/trackmate/cellpose/CellposeUtils.java
@@ -24,7 +24,23 @@ package fiji.plugin.trackmate.cellpose;
 import static fiji.plugin.trackmate.gui.GuiUtils.getResource;
 import static fiji.plugin.trackmate.gui.GuiUtils.scaleImage;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
 import javax.swing.ImageIcon;
+
+import ij.ImagePlus;
+import net.imagej.ImgPlus;
+import net.imagej.axis.Axes;
+import net.imglib2.Interval;
+import net.imglib2.img.display.imagej.ImageJFunctions;
+import net.imglib2.img.display.imagej.ImgPlusViews;
+import net.imglib2.type.NativeType;
+import net.imglib2.type.numeric.RealType;
+import net.imglib2.util.Intervals;
+import net.imglib2.view.IntervalView;
+import net.imglib2.view.Views;
 
 public class CellposeUtils
 {
@@ -47,5 +63,70 @@ public class CellposeUtils
 	public static final ImageIcon omniposeLogo64()
 	{
 		return scaleImage( omniposeLogo(), 64, 64 );
+	}
+
+	public static final < T extends RealType< T > & NativeType< T > > List< ImagePlus > crop( final ImgPlus< T > img, final Interval interval, final Function< Long, String > nameGen )
+	{
+		final int zIndex = img.dimensionIndex( Axes.Z );
+		final int cIndex = img.dimensionIndex( Axes.CHANNEL );
+		final Interval cropInterval;
+		if ( zIndex < 0 )
+		{
+			// 2D
+			if ( cIndex < 0 )
+				cropInterval = Intervals.createMinMax(
+						interval.min( 0 ), interval.min( 1 ),
+						interval.max( 0 ), interval.max( 1 ) );
+			else
+				// Include all channels
+				cropInterval = Intervals.createMinMax(
+						interval.min( 0 ), interval.min( 1 ), img.min( cIndex ),
+						interval.max( 0 ), interval.max( 1 ), img.max( cIndex ) );
+		}
+		else
+		{
+			if ( cIndex < 0 )
+				cropInterval = Intervals.createMinMax(
+						interval.min( 0 ), interval.min( 1 ), interval.min( 2 ),
+						interval.max( 0 ), interval.max( 1 ), interval.max( 2 ) );
+			else
+				cropInterval = Intervals.createMinMax(
+						interval.min( 0 ), interval.min( 1 ), interval.min( 2 ), img.min( cIndex ),
+						interval.max( 0 ), interval.max( 1 ), interval.max( 2 ), img.max( cIndex ) );
+		}
+
+		final List< ImagePlus > imps = new ArrayList<>();
+		final int timeIndex = img.dimensionIndex( Axes.TIME );
+		if ( timeIndex < 0 )
+		{
+			// No time.
+			final IntervalView< T > crop = Views.interval( img, cropInterval );
+			final String name = nameGen.apply( 0l ) + ".tif";
+			imps.add( ImageJFunctions.wrap( crop, name ) );
+		}
+		else
+		{
+			// In the interval, time is always the last.
+			final long minT = interval.min( interval.numDimensions() - 1 );
+			final long maxT = interval.max( interval.numDimensions() - 1 );
+			for ( long t = minT; t <= maxT; t++ )
+			{
+				final ImgPlus< T > tpTCZ = ImgPlusViews.hyperSlice( img, timeIndex, t );
+
+				// Put if necessary the channel axis as the last one (CellPose
+				// format)
+				final int chanDim = tpTCZ.dimensionIndex( Axes.CHANNEL );
+				ImgPlus< T > tp = tpTCZ;
+				if ( chanDim > 1 )
+				{
+					tp = ImgPlusViews.moveAxis( tpTCZ, chanDim, tpTCZ.numDimensions() - 1 );
+				}
+				// possibly 2D or 3D with or without channel.
+				final IntervalView< T > crop = Views.interval( tp, cropInterval );
+				final String name = nameGen.apply( t ) + ".tif";
+				imps.add( ImageJFunctions.wrap( crop, name ) );
+			}
+		}
+		return imps;
 	}
 }
