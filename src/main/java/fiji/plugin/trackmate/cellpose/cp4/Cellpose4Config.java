43faae7dd85dd3736d210fab179d1c2776409058
Jean-Yves Tinevez <tinevez@pasteur.fr>
2026-07-02 15:47:46 +0200

    Cellpose-SAM detector.

---
+++ src/main/java/fiji/plugin/trackmate/cellpose/cp4/Cellpose4Config.java
@@ -0,0 +1,73 @@
+package fiji.plugin.trackmate.cellpose.cp4;
+
+import org.scijava.ui.config.Parameters.IntParam;
+
+import fiji.plugin.trackmate.cellpose.CellposeBaseConfig;
+import net.imglib2.cellpose.Cellpose4BuiltinModels;
+
+public class Cellpose4Config extends CellposeBaseConfig< Cellpose4BuiltinModels >
+{
+
+	private final IntParam chan1;
+
+	private final IntParam chan2;
+
+	private final IntParam chan3;
+
+	public Cellpose4Config( final int nChannels, final double pixelSize, final String units )
+	{
+		super(
+				Cellpose4DetectorFactory.NAME,
+				Cellpose4DetectorFactory.DOC_CELLPOSE_URL,
+				Cellpose4BuiltinModels.class, pixelSize, units );
+
+		// Channels, two int params.
+		this.chan1 = addIntParameter()
+				.key( "CHAN1" )
+				.name( "Channel 1" )
+				.help( "First channel to include in the segmentation. Select 0 not use it." )
+				.defaultValue( 1 )
+				.min( 0 )
+				.max( nChannels )
+				.get();
+		this.chan2 = addIntParameter()
+				.key( "CHAN2" )
+				.name( "Channel 2" )
+				.help( "Second channel to include in the segmentation. Select 0 not use it." )
+				.defaultValue( 0 )
+				.min( 0 )
+				.max( nChannels )
+				.get();
+		this.chan3 = addIntParameter()
+				.key( "CHAN3" )
+				.name( "Channel 3" )
+				.help( "Third channel to include in the segmentation. Select 0 not use it." )
+				.defaultValue( 0 )
+				.min( 0 )
+				.max( nChannels )
+				.get();
+
+		// Change their order.
+		orderedElements.remove( chan1 );
+		orderedElements.add( 2, chan1 );
+		orderedElements.remove( chan2 );
+		orderedElements.add( 3, chan2 );
+		orderedElements.remove( chan3 );
+		orderedElements.add( 4, chan3 );
+	}
+
+	public IntParam chan1()
+	{
+		return chan1;
+	}
+
+	public IntParam chan2()
+	{
+		return chan2;
+	}
+
+	public IntParam chan3()
+	{
+		return chan3;
+	}
+}
---
+++ src/main/java/fiji/plugin/trackmate/cellpose/cp4/Cellpose4DetectorFactory.java
@@ -0,0 +1,169 @@
+/*-
+ * #%L
+ * TrackMate: your buddy for everyday tracking.
+ * %%
+ * Copyright (C) 2021 - 2026 TrackMate developers.
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+package fiji.plugin.trackmate.cellpose.cp4;
+
+import java.util.Map;
+
+import javax.swing.ImageIcon;
+
+import org.scijava.Priority;
+import org.scijava.plugin.Plugin;
+import org.scijava.ui.config.visitors.Maps;
+
+import fiji.plugin.trackmate.Model;
+import fiji.plugin.trackmate.Settings;
+import fiji.plugin.trackmate.cellpose.CellposeConfigPanel;
+import fiji.plugin.trackmate.cellpose.CellposeUtils;
+import fiji.plugin.trackmate.detection.SpotDetectorFactory;
+import fiji.plugin.trackmate.detection.SpotGlobalDetector;
+import fiji.plugin.trackmate.detection.SpotGlobalDetectorFactory;
+import fiji.plugin.trackmate.gui.components.ConfigurationPanel;
+import fiji.plugin.trackmate.util.TMUtils;
+import ij.ImagePlus;
+import net.imagej.ImgPlus;
+import net.imagej.axis.Axes;
+import net.imglib2.Interval;
+import net.imglib2.type.NativeType;
+import net.imglib2.type.numeric.RealType;
+
+@Plugin( type = SpotDetectorFactory.class, priority = Priority.LOW )
+public class Cellpose4DetectorFactory< T extends RealType< T > & NativeType< T > >
+		implements SpotGlobalDetectorFactory< T >
+{
+
+	/** A string key identifying this factory. */
+	public static final String DETECTOR_KEY = "CELLPOSE_SAM_DETECTOR";
+
+	/** The pretty name of the target detector. */
+	public static final String NAME = "Cellpose-SAM detector";
+
+	public static final String DOC_CELLPOSE_URL = "https://imagej.net/plugins/trackmate/detectors/trackmate-cellpose-sam";
+
+	/** An html information text. */
+	public static final String INFO_TEXT = "<html>"
+			+ "This detector relies on Cellpose-SAM to detect objects."
+			+ "<p>"
+			+ "The detector simply calls <b>Cellpose-SAM</b>, that will be installed on "
+			+ "your system via Appose. Appose will download and install Cellpose for you, "
+			+ "but it might take a few minutes the first time you use it."
+			+ "<p>"
+			+ "If you use this detector for your work, please be so kind as to "
+			+ "also cite the Cellpose SAM paper: "
+			+ "<a href=\"https://doi.org/10.1101/2025.04.28.651001\">"
+			+ "Marius Pachitariu, Michael Rariden, Carsen Stringer. "
+			+ "Cellpose-SAM: superhuman generalization for cellular segmentation. "
+			+ "bioRxiv 2025.04.28.651001"
+			+ "</a>"
+			+ "</html>";
+
+	@Override
+	public SpotGlobalDetector< T > getDetector( final ImgPlus< T > img, final Map< String, Object > settings, final Interval interval )
+	{
+		final Cellpose4Config config = createConfig( img );
+		Maps.fromMap( settings, config );
+		return new Cellpose4Detector< T >( img, interval, config );
+	}
+
+	private Cellpose4Config createConfig( final ImgPlus< ? > img )
+	{
+		final int nChannels = img.dimensionIndex( Axes.CHANNEL ) < 0 ? 1 : ( int ) img.dimension( img.dimensionIndex( Axes.CHANNEL ) );
+		final double pixelSize = img.averageScale( img.dimensionIndex( Axes.X ) );
+		final String units = img.axis( img.dimensionIndex( Axes.X ) ).unit();
+		return new Cellpose4Config( nChannels, pixelSize, units );
+	}
+
+	private Cellpose4Config createConfig( final Settings settings )
+	{
+		final ImagePlus imp = settings.imp;
+		if ( imp == null )
+			return new Cellpose4Config( 1, 1., "pixel" );
+		return createConfig( TMUtils.rawWraps( imp ) );
+	}
+
+	@Override
+	public ConfigurationPanel getDetectorConfigurationPanel( final Settings settings, final Model model )
+	{
+		return new CellposeConfigPanel( settings, model, createConfig( settings ), () -> this );
+	}
+
+	@Override
+	public Map< String, Object > getDefaultSettings()
+	{
+		final int nChannels = 3;
+		final double pixelSize = 1.;
+		final String units = "pixel";
+		final Cellpose4Config config = new Cellpose4Config( nChannels, pixelSize, units );
+		return Maps.toMap( config );
+	}
+
+	@Override
+	public boolean has2Dsegmentation()
+	{
+		return true;
+	}
+
+	@Override
+	public boolean has3Dsegmentation()
+	{
+		return true;
+	}
+
+	@Override
+	public String getInfoText()
+	{
+		return INFO_TEXT;
+	}
+
+	@Override
+	public String getKey()
+	{
+		return DETECTOR_KEY;
+	}
+
+	@Override
+	public String getName()
+	{
+		return NAME;
+	}
+
+	@Override
+	public String getUrl()
+	{
+		return DOC_CELLPOSE_URL;
+	}
+
+	@Override
+	public ImageIcon getIcon()
+	{
+		return CellposeUtils.cellposeLogo64();
+	}
+
+	@Override
+	public boolean forbidMultithreading()
+	{
+		/*
+		 * We want to run one frame after another, because the inference for one
+		 * frame takes all the resources anyway.
+		 */
+		return true;
+	}
+}
